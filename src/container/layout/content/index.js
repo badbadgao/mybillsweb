8a26d68fdf3d1f3254695fff7bbf9c6c17359700
badbadgao <wave.gao2013@gmail.com>
2019-05-13 21:48:29 +1200

    refactor/make clean code

--- src/container/layout/content/index.js
+++ src/container/layout/content/index.js
@@ -1,15 +1,16 @@
 // @flow
 
 import React from 'react';
-import { StyleSheet, css } from 'aphrodite';
-import { Layout, Breadcrumb, Button, Row } from 'antd';
-
-import BillTable from './BillTable';
-import { addBill, getBills, openAddBillModal, getProviders, getBillTypes, deleteBill } from 'reducers/bills/actions';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import AddBillModal from './AddBillModal';
 
+import { StyleSheet, css } from 'aphrodite';
+import { Layout, Breadcrumb} from 'antd';
+
+import BillTable from './billTable';
+import AddBillModal from './addBillModal';
+import HeadBar from './headBar';
+import { addBill, getBills, getProviders, getBillTypes } from 'reducers/bills/actions';
 const { Content } = Layout;
 
 const styles = StyleSheet.create({
@@ -31,40 +32,16 @@ class ContentBody extends React.Component<Props> {
     this.props.actions.getBillTypes();
   }
 
-  handleAdd = () => {
-    this.props.actions.openAddBillModal();
-  }
-
-  handleDelete = () => {
-    console.log("delete");
-    this.props.actions.deleteBill();
-  }
-  
   render() {
     return(
       <Layout style={{ padding: '0 24px 24px' }}>
         <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          {/* <Breadcrumb.Item>Home</Breadcrumb.Item>
           <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item> */}
         </Breadcrumb>
         <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
-          <Row>
-            <Button
-              onClick={this.handleAdd}
-              type="primary"
-              style={{ marginBottom: 16 }}>
-              Add Bill
-            </Button>
-            <Button 
-              onClick={this.handleDelete}
-              type="primary"
-              style={{ marginBottom: 16, marginLeft: 8 }}
-              disabled={this.props.deleteButtonDisabled}
-            >
-              Delete
-            </Button>
-          </Row>
+          <HeadBar />
           <BillTable />
           <AddBillModal/>
         </Content>
@@ -74,17 +51,15 @@ class ContentBody extends React.Component<Props> {
 };
 
 const mapStateToProps = state => ({
-  deleteButtonDisabled: !state.selectedRowsKeys || state.selectedRowsKeys.length == 0,
+  // deleteButtonDisabled: !state.selectedRowsKeys || state.selectedRowsKeys.length == 0,
 });
 
 const mapDispatchToProps = dispatch => ({
   actions: bindActionCreators({
     addBill,
     getBills,
-    openAddBillModal,
     getProviders,
     getBillTypes,
-    deleteBill,
   }, dispatch)
 });
 
